344c63ea9061786c45e8200f955ecfb22313fa44
Mike-Mp <devmichalis@protonmail.com>
2020-12-14 21:35:40 +0200

    add index,app.js's and controller folder

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
+const router = "./controller/routes.js";
+const path = require("path");
+
+app.use(express.json());
+
+app.use("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+console.log(__dirname + "public");
+// Routes
+app.use("/", router);
+
+// Error handling
 
 module.exports = app;
