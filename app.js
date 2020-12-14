00d9a09f5e5e1e7a0e39f39990151a493ef6c16c
Mike-Mp <devmichalis@protonmail.com>
2020-12-14 21:42:11 +0200

    bug fix in app, console.log change in index

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const router = "./controller/routes.js";
+const router = require("./controller/routes.js");
 const path = require("path");
 
 app.use(express.json());
 
-app.use("view engine", "pug");
+app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
 console.log(__dirname + "public");
--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const http = require("http");
 const server = http.createServer(app);
 
 server.listen(3001, () => {
-  console.log("server running on port 3001");
+  console.log(`
+  ${
+    process.env.NODE_ENV === "development"
+      ? "server is running on port 3001"
+      : ""
+  }
+  `);
 });
