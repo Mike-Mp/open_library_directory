37f9c0df3495a3a0892c551284531b0f9e4e5f36
Mike-Mp <devmichalis@protonmail.com>
2020-12-17 15:43:44 +0200

    use webpack to enable import statement support for slider.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,59 @@
 const path = require("path");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./public/js/slider.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "slider.js",
+    path: path.resolve(__dirname, "public/dist"),
   },
 
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              reloadAll: true,
+            },
+          },
+          "style-loader",
+          "css-loader",
+        ],
       },
       {
         test: /\.pug$/,
         use: "pug-loader",
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
     ],
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      path: require.resolve("path-browserify"),
+      url: require.resolve("url/"),
+      util: require.resolve("util/"),
+      stream: require.resolve("stream-browserify"),
+      http: require.resolve("stream-http"),
+      crypto: require.resolve("crypto-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+      assert: require.resolve("assert/"),
+    },
+  },
+  target: "node",
 };
