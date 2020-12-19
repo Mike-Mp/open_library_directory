07ea1cfc532995508368530510e2954c39bf6a41
Mike-Mp <devmichalis@protonmail.com>
2020-12-19 03:08:33 +0200

    add loop for slide li from object

--- controller/routes.js
+++ controller/routes.js
@@ -1,7 +1,25 @@
 const Router = require("express").Router();
 
+const slides = {
+  Art: "fas fa-brush",
+  "Science Fiction": "fas fa-user-astronaut",
+  Fantasy: "fas fa-dragon",
+  Biographies: "fas fa-atlas",
+  Recipes: "fas fa-utensils",
+  Romance: "fas fa-heart",
+  Textbooks: "fas fa-book-open",
+  Children: "fas fa-child",
+  History: "fas fa-book",
+  Medicine: "fas fa-clinic-medical",
+  Religion: "fas fa-pray",
+  "Mystery Stories": "fas fa-book-dead",
+  Plays: "fas fa-theater-masks",
+  Music: "fas fa-music",
+  Science: "fas fa-atom",
+};
+
 Router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { slides });
 });
 
 module.exports = Router;
