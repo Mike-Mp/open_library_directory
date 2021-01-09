569bb011b3ec14f0060433a65a9e50d232457fb3
Mike-Mp <devmichalis@protonmail.com>
2021-01-09 16:50:58 +0200

    add redis to app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const router = require("./controller/routes.js");
 const path = require("path");
 const middleware = require("./utils/middleware");
 const compression = require("compression");
+const responseTime = require("response-time");
+const redis = require("redis");
 
 app.use(express.json());
 
@@ -14,6 +16,15 @@ app.set("view engine", "pug");
 app.use(express.static("public", { maxAge: 3600000 }));
 
 app.use(compression({ level: 6 }));
+
+const client = redis.createClient({
+  host: "localhost",
+  port: process.env.REDIS_PORT,
+});
+
+if (NODE_ENV !== "development") {
+  app.use(responseTime());
+}
 
 // Routes
 app.use("/", router);
